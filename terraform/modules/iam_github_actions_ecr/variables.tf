d25afd92b6c02b1071c9f2699e6b0b4d274c040f
quasar0x <100059333+quasar0x@users.noreply.github.com>
2026-01-09 22:20:22 +0100

    updated gitaction config

---
+++ terraform/modules/iam_github_actions_ecr/variables.tf
@@ -0,0 +1,31 @@
+variable "github_org" {
+  type        = string
+  description = "GitHub org/user name, e.g. quasar0x"
+}
+
+variable "github_repo" {
+  type        = string
+  description = "GitHub repo name, e.g. fintech-platform-infra"
+}
+
+variable "role_name" {
+  type        = string
+  description = "IAM role name for GitHub Actions"
+  default     = "github-actions-ecr-push"
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "AWS Account ID"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+  default     = "us-east-1"
+}
+
+variable "ecr_repository_name" {
+  type        = string
+  description = "ECR repository name to allow push to (least privilege)"
+}
